c7b5d81303da3b81891e161e5a38566ad8046ee2
BinNDL <binndl@liftsoft.vn>
2024-01-16 19:48:16 +0700

    update use database on cloud

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,15 @@
-const { default: mongoose } = require("mongoose")
+const mongoose = require("mongoose");
 
-const dbConnect = () => {
+const dbConnect = async () => {
     try {
-        const conn = mongoose.connect(process.env.MONGODB_URL);
-        console.log("Database Connect Successfully");
+        await mongoose.connect("mongodb+srv://lebin642:binbep25112201@cluster0.b5qkn1e.mongodb.net/FD_Database?retryWrites=true&w=majority", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Database Connected Successfully");
     } catch (error) {
-        console.log("Database Error");
+        console.error("Database Connection Error:", error.message);
     }
 };
+
 module.exports = dbConnect;
